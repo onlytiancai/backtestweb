710c77af55f3f4309e982681e8f70c5862ac0a28
Your Name <you@example.com>
2022-07-02 11:55:19 +0800

    test ok

--- app.py
+++ app.py
@@ -2,21 +2,104 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import backtrader as bt
 import datetime
+from io import BytesIO
+import base64
+from matplotlib import pyplot as plt 
+plt.rcParams['figure.figsize'] = (16, 6)
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# Create a Stratey
 class TestStrategy(bt.Strategy):
+    params = (
+        ('maperiod', 15),
+    )
 
     def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
+        emit('strategy-log',  '%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
     def next(self):
+        # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] > self.sma[0]:
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.dataclose[0] < self.sma[0]:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
 
 
 def run_strategy():
@@ -26,7 +109,7 @@ def run_strategy():
     data = bt.feeds.GenericCSVData(
         dataname='testdata/000300.2002-2022.csv',
 
-        fromdate=datetime.datetime(2022, 4, 8),
+        fromdate=datetime.datetime(2015, 4, 8),
         todate=datetime.datetime(2022, 6, 1),
 
         nullvalue=0.0,
@@ -48,14 +131,23 @@ def run_strategy():
     cerebro.run()
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
+    fig = cerebro.plot()[0][0]
+    save_file = BytesIO()
+    fig.savefig(save_file, format='png')
+    save_file_base64 = base64.b64encode(save_file.getvalue()).decode('utf8')
+    emit('plot',  save_file_base64)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('connect')
 def test_connect():
+    emit('after connect',  {'data':'Lets run a strategy'})
+
+@socketio.on('start')
+def handle_my_custom_event(json):
     run_strategy()
-    emit('after connect',  {'data':'Lets dance'})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8000)
